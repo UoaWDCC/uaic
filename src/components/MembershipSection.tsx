1e76fc6aa5ae3a92167229495cb14e3a760e70b1
andrecamerino <camerinoandre@gmail.com>
2025-05-01 11:05:38 +1200

    fix Membership Section (component naming + tailwind design)

--- src/components/MembershipSection.tsx
+++ src/components/MembershipSection.tsx
@@ -2,15 +2,35 @@
 
 import React from "react";
 
-const MembershipCallout = () => {
+const MembershipSection = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-4 ">
-      <h1 className="w-[281px] lg:w-[388px] text-center text-[16px]/[20px] lg:text-[30px]/[35px]">
+    <div
+      className="
+      flex flex-col justify-center items-center gap-4
+    ">
+      <h1
+        className="
+        w-[281px] 
+        text-center text-[16px]/[20px]
+
+        lg:w-[388px] lg:text-[30px]/[35px]
+      ">
         Click to learn more about our Membership
       </h1>
-        <a href="/memberships" className="bg-[#EBF7FE] hover:bg-[#145CA9] hover:text-white transition px-4 lg:py-2 rounded-[40px] text-[14px]/[35px] lg:text-[25px]/[35px]">Membership</a>
+      <a
+        href="/memberships"
+        className="
+        bg-[#EBF7FE]
+        px-4 rounded-[40px] 
+        text-[14px]/[35px] 
+        transition hover:text-white hover:bg-[#145CA9] active:bg-[#0f4a88]
+        
+        lg:py-2 lg:text-[25px]/[35px]   
+      ">
+        Membership
+      </a>
     </div>
   );
 };
 
-export default MembershipCallout;
+export default MembershipSection;
