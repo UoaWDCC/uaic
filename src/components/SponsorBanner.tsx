e85a6a0b11505eab3ce46024fb3868df2731a6ea
andrecamerino <camerinoandre@gmail.com>
2025-05-01 12:39:10 +1200

    fix sponsor banner (component naming + tailwind design)

--- src/components/SponsorBanner.tsx
+++ src/components/SponsorBanner.tsx
@@ -1,30 +1,78 @@
 import React from "react";
 import Image from "next/image";
 
-const Partnership = () => {
+const SponsorBanner = () => {
   return (
-    <div className="w-full flex flex-col items-center gap-[20px] text-pretty">
-      <h1 className="font-[400] text-[10px] lg:text-[28px] w-[313px] lg:w-[712px] text-center">
+    <div
+      className="
+      w-full 
+      flex flex-col items-center gap-[20px] 
+      text-pretty
+    "
+    >
+      <h1
+        className="
+        w-[313px] 
+        font-[400] text-[10px] text-center
+
+        lg:text-[28px]
+        lg:w-[712px] 
+      "
+      >
         THE UAIC BULLETIN IS BROUGHT TO YOU IN PARTNERSHIP WITH:
       </h1>
-      <div className="w-full h-[34px] lg:h-[54px] bg-[#D9D9D9] flex justify-evenly items-center">
+      <div
+        className="
+        w-full h-[34px] 
+        flex justify-evenly items-center
+        bg-[#D9D9D9] 
+
+        lg:h-[54px] 
+      "
+      >
         <Image
           src="/assets/forsyth-barr-logo.png"
           alt="forsyth-barr"
           width={100}
           height={100}
-          className="w-[160px] lg:w-[254px] h-[20px] lg:h-[32px] object-contain"
+          className="
+            w-[160px] h-[20px] 
+            object-contain
+            
+            lg:w-[254px] 
+            lg:h-[32px] 
+        "
         />
         <Image
           src="/assets/NBR-logo.png"
           alt="NBR"
           width={100}
           height={100}
-          className="w-[70px] lg:w-[112px] h-[20px] lg:h-[32px] object-contain"
+          className="
+            w-[70px] 
+            h-[20px] 
+            object-contain
+
+            lg:w-[112px] 
+            lg:h-[32px] 
+        "
         />
       </div>
-      <div className="border p-4 lg:p-14 lg:px-30 m-4 mt-14 max-w-[773px]">
-        <p className="text-[8px]/[22px] lg:text-[18px]/[22px]">
+      <div
+        className="
+        p-4 m-4 mt-14 max-w-[773px]
+        border 
+
+        lg:p-14 lg:px-30 
+      "
+      >
+        <p
+          className="
+          text-[8px]/[22px] 
+          
+          lg:text-[18px]/[22px]
+        "
+        >
           If earnestly extremity he he propriety something admitting convinced
           ye. Pleasant in to although as if differed horrible. Mirth his quick
           its set front enjoy hoped had there. Who connection imprudence
@@ -37,4 +85,4 @@ const Partnership = () => {
   );
 };
 
-export default Partnership;
+export default SponsorBanner;
