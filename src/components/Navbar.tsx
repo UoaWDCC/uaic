6d899a1fcac0ab23cf5722e7bfd7d12fa324d682
Evan Au <evan.au206@gmail.com>
2025-05-09 15:28:29 +1200

    applied figma styling changes, breakpoint changed from sm to lg to fix scaling issues.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,10 +39,10 @@ const Navbar = () => {
 
 
      {/* Top Bar */}
-     <div className="bg-white sm:bg-[#F0F8FF] text-[#172741] p-4 flex justify-between items-center">
+     <div className="bg-white lg:bg-[#F0F8FF] text-[#172741] p-4 flex justify-between items-center">
        {/* Hamburger Menu */}
        <button
-         className="sm:hidden"
+         className="lg:hidden"
          onClick={() => setIsOpen(!isOpen)}
        >
          <svg
@@ -61,19 +61,19 @@ const Navbar = () => {
        </button>
 
        {/* Desktop Links */}
-       <ul className="hidden sm:flex gap-4 justify-center flex-grow order-2">
-         <li><a href="/" className="hover:underline hover:bg-[#808080] hover:rounded-xl hover:text-[#fff] p-[4px]">Home</a></li>
-         <li><a href="/about" className="hover:underline hover:bg-[#808080] hover:rounded-xl hover:text-[#fff] p-[4px]">About</a></li>
-         <li><a href="/events" className="hover:underline hover:bg-[#808080] hover:rounded-xl hover:text-[#fff] p-[4px]">Events</a></li>
-         <li><a href="/investmentportfolio" className="hover:underline hover:bg-[#808080] hover:rounded-xl hover:text-[#fff] p-[4px]">Investment Portfolio</a></li>
-         <li><a href="/bulletin" className="hover:underline hover:bg-[#808080] hover:rounded-xl hover:text-[#fff] p-[4px]">Bulletin</a></li>
-         <li><a href="/contact" className="hover:underline hover:bg-[#808080] hover:rounded-xl hover:text-[#fff] p-[4px]">Contact</a></li>
-       </ul>
+       {/* Desktop Links */}
+      <ul className="hidden lg:flex gap-4 justify-center flex-grow order-2">
+        <li><a href="/" className="hover:text-[#145CA9] hover:rounded-xl p-[4px] font-[100]">Home</a></li>
+        <li><a href="/about" className="hover:text-[#145CA9] hover:rounded-xl p-[4px] font-[300]">About</a></li>
+        <li><a href="/events" className="hover:text-[#145CA9] hover:rounded-xl p-[4px] font-[300]">Events</a></li>
+        <li><a href="/investmentportfolio" className="hover:text-[#145CA9] hover:rounded-xl p-[4px] font-[300]">Investment Portfolio</a></li>
+        <li><a href="/bulletin" className="hover:text-[#145CA9] hover:rounded-xl p-[4px] font-[300]">Bulletin</a></li>
+        <li><a href="/contact" className="hover:text-[#145CA9] hover:rounded-xl p-[4px] font-[300]">Contact</a></li>
+      </ul>
 
-       {/* Sign Up and Login */}
-       <ul className="hidden sm:flex gap-4 ml-auto order-3">
-         <li><a href="/signup" className="hover:underline hover:bg-[#808080] bg-[#fff] rounded-xl hover:text-[#fff] p-[4px]">Sign Up</a></li>
-         <li><a href="/login" className="hover:underline hover:bg-[#808080] bg-[#fff] rounded-xl hover:text-[#fff] p-[4px]">Login</a></li>
+       {/* Join Us */}
+       <ul className="hidden lg:flex gap-4 ml-auto order-3 p-[10px]">
+         <li><a href="/joinus" className="p-[10px] border border-solid border-[#145CA9] hover:bg-[#fff] bg-[#145CA9] rounded-4xl text-[#fff] hover:text-[#145CA9] px-[20] py-[6] font-[600] text-[14px]">Join Us</a></li>
        </ul>
 
        {/* Logo */}
@@ -100,15 +100,15 @@ const Navbar = () => {
 
      {/* Mobile Menu */}
      {isOpen && (
-       <ul className="h-screen bg-[#145CA9] text-white flex-auto flex-col gap-4 sm:hidden w-full">
-         <li className="p-4 border-b border-white"><a href="/" className="hover:underline">Home</a></li>
-         <li className="p-4 border-b border-white"><a href="/about" className="hover:underline">About</a></li>
-         <li className="p-4 border-b border-white"><a href="/events" className="hover:underline">Events</a></li>
-         <li className="p-4 border-b border-white"><a href="/investmentportfolio" className="hover:underline">Investment Portfolio</a></li>
-         <li className="p-4 border-b border-white"><a href="/bulletin" className="hover:underline">Bulletin</a></li>
-         <li className="p-4 border-b border-white"><a href="/contact" className="hover:underline">Contact</a></li>
-         <li className="p-4 border-b border-white"><a href="/signup" className="hover:underline">Sign Up</a></li>
-         <li className="p-4"><a href="/login" className="hover:underline">Login</a></li>
+       <ul className="h-screen bg-[#145CA9] text-white flex-auto flex-col gap-4 lg:hidden w-full">
+         <li className="p-4 border-b border-white"><a href="/" className="">Home</a></li>
+         <li className="p-4 border-b border-white"><a href="/about" className="">About</a></li>
+         <li className="p-4 border-b border-white"><a href="/events" className="">Events</a></li>
+         <li className="p-4 border-b border-white"><a href="/investmentportfolio" className="">Investment Portfolio</a></li>
+         <li className="p-4 border-b border-white"><a href="/bulletin" className="">Bulletin</a></li>
+         <li className="p-4 border-b border-white"><a href="/contact" className="">Contact</a></li>
+         <li className="p-4 border-b border-white"><a href="/signup" className="">Sign Up</a></li>
+         <li className="p-4"><a href="/login" className="">Login</a></li>
        </ul>
      )}
    </nav>
