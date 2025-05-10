915923f04445d683e95e0db013905984afe6b97f
Sam Richell-Smith <samrs6012@gmail.com>
2025-05-10 16:27:30 +1200

    update tailwind classes to organize by styling breakpoints

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,23 +3,34 @@ import { FaInstagram, FaLinkedin, FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-[#145CA9] lg:p-10  flex flex-col items-center justify-center gap-8 pb-6 pt-4">
-      <div className="p-4 w-full grid grid-cols-1 lg:grid-cols-4 items-center text-center gap-y-8 gap-x-12 px-16 pb-0">
+    <footer className="w-full flex flex-col items-center justify-center gap-8 pb-6 pt-4
+    bg-[#145CA9] 
+    lg:p-10 ">
+      <div className="p-4 w-full grid grid-cols-1 items-center text-center gap-y-8 gap-x-12 px-16 pb-0
+      lg:grid-cols-4
+      ">
         <div className="max-w-[20rem]  justify-self-center">
           <img src="/assets/uaic-white.png" alt="" />
         </div>
 
-        <h1 className="hidden lg:block text-[2rem] lg:text-[1.4rem] xl:text-[1.8rem] 2xl:text-[2rem] justify-self-center whitespace-nowrap">
+        <h1 className="hidden text-[2rem]justify-self-center whitespace-nowrap
+        lg:block lg:text-[1.4rem]  xl:text-[1.8rem] 2xl:text-[2rem] 
+        ">
           Investing is for Everyone
         </h1>
 
-        <h1 className="text-lg lg:text-[1.4rem] xl:text-[1.8rem] 2xl:text-[2rem] justify-self-center order-1 lg:order-2">
+        <h1 className="text-lg justify-self-center order-1 
+         lg:text-[1.4rem] xl:text-[1.8rem] 2xl:text-[2rem] lg:order-2
+        ">
           Email Address
         </h1>
 
-        <div className="grid grid-cols-4 lg:grid-cols-2 lg:gap-8 gap-12 justify-self-center order-2 lg:order-1">
+        <div className="grid grid-cols-4 gap-12 justify-self-center order-2 
+        lg:grid-cols-2 lg:gap-8  lg:order-1
+        ">
           <a
-            className="hover:scale-105 hover:text-white transition-all duration-250 order-2 lg:order-1"
+            className="hover:scale-105 hover:text-white transition-all duration-250 order-2 
+            lg:order-1"
             href="https://www.instagram.com/officialuaic/"
             target="_blank"
             rel="noopener noreferrer"
@@ -28,7 +39,8 @@ const Footer = () => {
             <FaInstagram className="size-12" />
           </a>
           <a
-            className="hover:scale-105 thover:scale-110 hover:text-white transition-all duration-250 order-3 lg:order-2"
+            className="hover:scale-105 thover:scale-110 hover:text-white transition-all duration-250 order-3 
+            lg:order-2"
             href="https://www.linkedin.com/company/officialuaic/"
             target="_blank"
             rel="noopener noreferrer"
@@ -39,7 +51,8 @@ const Footer = () => {
 
           {/* Not sure if this mail link works, this is what their current website links to */}
           <a
-            className="hover:scale-105 hover:text-white transition-all duration-250 order-4 lg:order-3"
+            className="hover:scale-105 hover:text-white transition-all duration-250 order-4 
+            lg:order-3"
             href="mailto:president@uaic.co.nz"
             target="_blank"
             rel="noopener noreferrer"
@@ -48,7 +61,8 @@ const Footer = () => {
             <MdOutlineMail className="size-12" />
           </a>
           <a
-            className="hover:scale-105 hover:text-white transition-all duration-250 order-1 lg:order-4"
+            className="hover:scale-105 hover:text-white transition-all duration-250 order-1 
+            lg:order-4"
             href="https://www.facebook.com/officialUAIC"
             target="_blank"
             rel="noopener noreferrer"
