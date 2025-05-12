0c464dfd5b11230b66f926d8bc200d581c29025f
ntur101 <ntur101@aucklanduni.ac.nz>
2025-05-13 11:36:55 +1200

    added in all the mobile first design stuff so it resizes good now 📱🔥

--- src/components/Bulletin.tsx
+++ src/components/Bulletin.tsx
@@ -6,13 +6,17 @@ const Bulletin = () => {
     <div
       className="
         grid 
-        grid-cols-[400px_1fr] grid-rows-2 
+        grid-cols-[33.33333%_1fr] grid-rows-2 
         gap-[10px] 
         w-full
-        h-[300px]
+        h-[150px]
+        text-[10px]/[12px] 
+
+        lg:h-[300px]
+        lg:text-[18px]/[22px]
       "
     >
-      <div className="bg-[#EBF7FE] flex items-center justify-center text-black text-sm lg:text-l w-[400px]">
+      <div className="bg-[#EBF7FE] flex items-center justify-center text-black">
         <Image
           src="/assets/stonks-up.jpg"
           alt="forsyth-barr"
@@ -25,14 +29,26 @@ const Bulletin = () => {
           "
         />
       </div>
-      <div className="bg-[#EBF7FE] flex items-center justify-center text-black text-sm lg:text-l">
+      <div className="bg-[#EBF7FE] flex items-center justify-center text-black p-4 lg:p-10 ">
+        If earnestly extremity he he propriety something admitting convinced
+        ye. Pleasant in to although as if differed horrible. Mirth his quick
+        its set front enjoy hoped had there.
+      </div>
+      <div className="bg-[#EBF7FE] flex items-center justify-center text-black ">
+      <Image
+          src="/assets/stonks-up.jpg"
+          alt="forsyth-barr"
+          width={100}
+          height={100}
+          className="
+            w-full
+            h-full
+            object-contain
+          "
+        />
+      </div>
+      <div className="bg-[#EBF7FE] flex items-center justify-center text-black p-4 lg:p-10">
         stonks
-      </div>
-      <div className="bg-[#EBF7FE] flex items-center justify-center text-black text-sm lg:text-l w-[400px]">
-        Image 2 goes here
-      </div>
-      <div className="bg-[#EBF7FE] flex items-center justify-center text-black text-sm lg:text-l">
-        Image 2 Description
       </div>
     </div>
   );
