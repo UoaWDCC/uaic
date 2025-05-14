0e2392c27bcbf032bb1fa7540de25cc6720f545e
ntur101 <ntur101@aucklanduni.ac.nz>
2025-05-14 12:17:31 +1200

    updated the structure of my code so it looks a lot cleaner now 🧹

--- src/components/Bulletin.tsx
+++ src/components/Bulletin.tsx
@@ -10,8 +10,8 @@ const Bulletin = () => {
       text-pretty
     "
     >
-    <div
-      className="
+      <div
+        className="
         grid 
         grid-cols-[33.33333%_1fr] grid-rows-2 
         gap-[10px] 
@@ -22,70 +22,54 @@ const Bulletin = () => {
         lg:h-[300px]
         lg:text-[22px]/[22px]
       "
-    >
-      <div className="bg-[#EBF7FE] flex items-center justify-center text-black">
-        <Image
-          src="/assets/stonks-up.jpg"
-          alt="forsyth-barr"
-          width={100}
-          height={100}
-          className="
+      >
+        <div className="bg-[#EBF7FE] flex items-center justify-center text-black">
+          <Image
+            src="/assets/stonks-up.jpg"
+            alt="forsyth-barr"
+            width={100}
+            height={100}
+            className="
             w-full
             h-full
             object-contain
           "
-        />
-      </div>
-      <div className="bg-[#EBF7FE] flex items-center justify-center text-black p-4 lg:p-10 ">
-        If earnestly extremity he he propriety something admitting convinced
-        ye. Pleasant in to although as if differed horrible. Mirth his quick
-        its set front enjoy hoped had there.
-      </div>
-      <div className="bg-[#EBF7FE] flex items-center justify-center text-black ">
-      <Image
-          src="/assets/stonks-up.jpg"
-          alt="forsyth-barr"
-          width={100}
-          height={100}
-          className="
+          />
+        </div>
+        <div className="bg-[#EBF7FE] flex items-center justify-center text-black p-4 lg:p-10 ">
+          If earnestly extremity he he propriety something admitting convinced
+          ye. Pleasant in to although as if differed horrible. Mirth his quick
+          its set front enjoy hoped had there.
+        </div>
+        <div className="bg-[#EBF7FE] flex items-center justify-center text-black ">
+          <Image
+            src="/assets/stonks-up.jpg"
+            alt="forsyth-barr"
+            width={100}
+            height={100}
+            className="
             w-full
             h-full
             object-contain
           "
-        />
+          />
+        </div>
+        <div className="bg-[#EBF7FE] flex items-center justify-center text-black p-4 lg:p-10">
+          stonks
+        </div>
       </div>
-      <div className="bg-[#EBF7FE] flex items-center justify-center text-black p-4 lg:p-10">
-        stonks
+      
+      <div className="w-full h-[75px] lg:h-[200px] flex justify-center items-center">
+        <div className="text-center px-2">
+          <h1 className="text-[10px] lg:text-[28px] font-normal">
+            Invest, Connect and Work with Us
+          </h1>
+          <p className="text-[8px] lg:text-[20px] font-normal mt-1">
+            Embark on a Rewarding Journey
+          </p>
+        </div>
       </div>
-    </div>
-    <div className="w-full h-[75px] flex justify-center items-center lg:h-[200px]">
-    <div>
-    <h1
-        className="
-        p-2
-        font-[400] text-[10px] text-center
 
-        lg:text-[28px]
-      "
-      >
-        Invest, Connect and Work with Us
-      </h1>
-      <div>
-    <p
-        className="
-        p-2
-        font-[400] text-[8px] text-center
-
-        lg:text-[20px]
-      "
-      >
-        Embark on a Rewarding Journey
-      </p>
-    </div>
-    </div>
-    
-    
-    </div>
     </div>
   );
 };
