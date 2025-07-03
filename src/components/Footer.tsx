b7e3414f233cb704d8fc1236cea03911b70d5d3a
Sam Richell-Smith <samrs6012@gmail.com>
2025-07-03 13:40:56 +1200

    Change to 3 column system

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,25 +7,29 @@ const Footer = () => {
     bg-[#145CA9] text-white
     lg:p-10 ">
       <div className="p-4 w-full grid grid-cols-1 items-center text-center gap-y-8 gap-x-12 px-16 pb-0
-      lg:grid-cols-4
+      lg:grid-cols-3
       ">
+
+        {/* Col 1 */}
         <div className="max-w-[20rem]  justify-self-center">
           <img src="/assets/uaic-white.png" alt="" />
         </div>
 
+        {/* Col 2 */}
         <h1 className="hidden text-[2rem]justify-self-center whitespace-nowrap
         lg:block lg:text-[1.4rem]  xl:text-[1.8rem] 2xl:text-[2rem] 
         ">
           Investing is for Everyone
         </h1>
 
+        {/* Col 3 */}
         <h1 className="text-lg justify-self-center order-1 
          lg:text-[1.4rem] xl:text-[1.8rem] 2xl:text-[2rem] lg:order-2
         ">
           Email Address
         </h1>
 
-        <div className="grid grid-cols-4 gap-12 justify-self-center order-2 
+        {/* <div className="grid grid-cols-4 gap-12 justify-self-center order-2 
         lg:grid-cols-2 lg:gap-8  lg:order-1
         ">
           <a
@@ -49,7 +53,7 @@ const Footer = () => {
             <FaLinkedin className="size-11" />
           </a>
 
-          {/* Not sure if this mail link works, this is what their current website links to */}
+          
           <a
             className="hover:scale-105 hover:text-white transition-all duration-250 order-4 
             lg:order-3"
@@ -70,7 +74,7 @@ const Footer = () => {
           >
             <FaFacebook className="size-11" />
           </a>
-        </div>
+        </div> */}
       </div>
 
       <h1 className="lg:text-[1.6rem] ">Copyright 2025 or something</h1>
