d5844c79f29a3152870a2cc2921b04d6fd33123c
Sam Richell-Smith <samrs6012@gmail.com>
2025-07-03 16:03:17 +1200

    change copyright section content and column spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,31 +3,64 @@ import { FaInstagram, FaLinkedin, FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="w-full flex flex-col items-center justify-center gap-8 pb-6 pt-4
-    bg-[#145CA9] text-white
-    lg:p-10 ">
-      <div className="p-4 w-full grid grid-cols-1 items-center text-center gap-y-8 gap-x-12 px-16 pb-0
-      lg:grid-cols-3
-      ">
-
+    <footer
+      className="w-full flex flex-col items-center justify-center gap-8 pb-6 pt-4
+    bg-darkBlue text-white
+    lg:p-10 "
+    >
+      <div
+        className="p-4 w-full grid grid-cols-1 items-start text-center gap-y-8 gap-x-12 px-16 pb-0
+      lg:grid-cols-3 pl-12 pr-12
+      "
+      >
         {/* Col 1 */}
-        <div className="max-w-[20rem]  justify-self-center">
-          <img src="/assets/uaic-white.png" alt="" />
+        <div className="flex flex-col items-start">
+          <h1 className="hidden lg:block text-[2rem] lg:text-[1.4rem] xl:text-[2.5rem] 2xl:text-[4rem] font-bold whitespace-nowrap">
+            Links
+          </h1>
+
+          <div className="hidden lg:flex lg:flex-col items-start text-left underline text-[2rem] lg:text-[1.4rem] xl:text-[1.4rem] 2xl:text-[2rem] font-light whitespace-nowrap">
+            <h2>Home</h2>
+            <h2>About</h2>
+            <h2>Events</h2>
+            <h2>Investments</h2>
+            <h2>Articles</h2>
+            <h2>Contact</h2>
+          </div>
         </div>
 
         {/* Col 2 */}
-        <h1 className="hidden text-[2rem]justify-self-center whitespace-nowrap
-        lg:block lg:text-[1.4rem]  xl:text-[1.8rem] 2xl:text-[2rem] 
-        ">
-          Investing is for Everyone
-        </h1>
+         <div className="flex flex-col items-start">
+          <h1 className="hidden lg:block text-[2rem] lg:text-[1.4rem] xl:text-[2.5rem] 2xl:text-[4rem] font-bold whitespace-nowrap">
+            Contact Us
+          </h1>
+
+          <div className="hidden lg:flex lg:flex-col items-start text-left underline text-[2rem] lg:text-[1.4rem] xl:text-[1.4rem] 2xl:text-[2rem] font-light whitespace-nowrap">
+            <h2>Home</h2>
+            <h2>About</h2>
+            <h2>Events</h2>
+            <h2>Investments</h2>
+            <h2>Articles</h2>
+            <h2>Contact</h2>
+          </div>
+        </div>
 
         {/* Col 3 */}
-        <h1 className="text-lg justify-self-center order-1 
-         lg:text-[1.4rem] xl:text-[1.8rem] 2xl:text-[2rem] lg:order-2
-        ">
-          Email Address
-        </h1>
+         <div className="flex flex-col items-start">
+          <h1 className="hidden lg:block text-[2rem] lg:text-[1.4rem] xl:text-[2.5rem] 2xl:text-[4rem] font-bold whitespace-nowrap">
+            Follow Us
+          </h1>
+
+          <div className="hidden lg:flex lg:flex-col items-start text-left underline text-[2rem] lg:text-[1.4rem] xl:text-[1.4rem] 2xl:text-[2rem] font-light whitespace-nowrap">
+            <h2>Home</h2>
+            <h2>About</h2>
+            <h2>Events</h2>
+            <h2>Investments</h2>
+            <h2>Articles</h2>
+            <h2>Contact</h2>
+          </div>
+        </div>
+        
 
         {/* <div className="grid grid-cols-4 gap-12 justify-self-center order-2 
         lg:grid-cols-2 lg:gap-8  lg:order-1
@@ -77,7 +110,16 @@ const Footer = () => {
         </div> */}
       </div>
 
-      <h1 className="lg:text-[1.6rem] ">Copyright 2025 or something</h1>
+      <hr className="w-[100%] border-t border-gray-300 my-1" />
+
+      <div className="flex justify-between w-full pl-12 pr-12">
+        
+        
+        <h1 className="lg:text-[1.4rem] ">© Copyright 2025 University of Auckland Investment Club</h1>
+        <div className="max-w-[8rem]  justify-self-center">
+          <img src="/assets/uaic-white.png" alt="" />
+        </div>
+      </div>
     </footer>
   );
 };
