fb1db2e18c243af1c8c881487993f62dbe00839f
paigephan <ppha961@aucklanduni.ac.nz>
2025-07-07 23:23:01 +1200

    update bugs

--- src/components/SponsorsBanner.tsx
+++ src/components/SponsorsBanner.tsx
@@ -5,20 +5,17 @@ import Image from "next/image";
 const SponsorsBanner = () => {
   return (
     <>
-      {/* This is to hide/disable the scrolling horizontally of the background */}
-    
       <div className="
             relative
             h-[178px] lg:h-[229px]
-            bg-gradient-to-r from-white/70 via-white/30 to-transparent
-            rounded-[8px]
-            shadow-[0_0_30px_rgba(255,255,255,0.4)]   // subtle glow
+            bg-white
             overflow-hidden
             ">
 
         <h3 className="
-                        mt-[40px] font-[600] text-[#145CA9] 
+                        mt-[0px] font-[600] text-[#145CA9] 
                         text-[19.04px] leading-[19.04px] text-center
+                        lg:mt-[40px]
                         lg:text-[28px] lg:leading-[22px]
                         ">
                         Our Sponsors
@@ -29,25 +26,25 @@ const SponsorsBanner = () => {
             h-[25.14px] w-[285px] mt-[60px]
             mx-auto 
             gap-[62px]
-            lg:gap-[376px]
+            lg:gap-[406px]
             lg:h-[52px]
             lg:w-[971.4px]
             "
         >
             <Image
-            src="/assets/forsyth-barr-logo-v2.png"
+            src="/assets/forsyth-barr-logo-v1.png"
             alt="forsyth-barr"
             width={135}
             height={16.95}
             className="
                 object-contain 
-                lg:w-[413.4px] 
-                lg:h-[51.92px]
+                lg:w-[500px] 
+                lg:h-[280px]
             "
             />
 
             <Image
-            src="/assets/NBR-logo-v2.png"
+            src="/assets/NBR-logo-v1.png"
             alt="NBR"
             width={88}
             height={25.14}
