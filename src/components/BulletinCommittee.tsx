8be89a749ab11bf984f2c4053b35d1858f28931b
Sam Richell-Smith <samrs6012@gmail.com>
2025-07-10 16:57:03 +1200

    add inner drop down

--- src/components/BulletinCommittee.tsx
+++ src/components/BulletinCommittee.tsx
@@ -2,38 +2,75 @@
 import React, { useState } from "react";
 import Image from "next/image";
 
-
 const InvestmentCommittee = () => {
   const [isMainOpen, setIsMainOpen] = useState(false);
+  const [isMembersOpen, setIsMembersOpen] = useState(false);
 
+  type Member = {
+  name: string;
+  role: string;
+};
+
+const members: Member[] = [
+    { name: "Riley Bogard-Allen", role: "Editor-in-Chief" },
+    { name: "Isabella Ho", role: "Sub-Editor" },
+    { name: "Devika Modak", role: "Senior Writer" },
+    { name: "Daniel Gunho-Song", role: "Writer" },
+    { name: "Yasmin Coombe", role: "Writer" },
+    { name: "Andy Tran", role: "Writer" },
+    { name: "Meghana Gaddam", role: "Writer" },
+    { name: "Connor Petrie", role: "Writer" },
+    { name: "Yao Ming Lee", role: "Writer" },
+    { name: "Alice Hoult", role: "Writer" },
+    { name: "Waejen Kwan", role: "Writer" },
+    { name: "Bella Crawford", role: "Writer" },
+    { name: "Sohum Karl", role: "Writer" },
+    { name: "Oliver Turnbull", role: "Writer" },
+    { name: "Lisa Shiozawa", role: "Writer" },
+    { name: "Hunter Sari", role: "Writer" },
+    { name: "Manav Treekam", role: "Writer" },
+    { name: "Annabelle Larsen", role: "Writer" }
+];
 
 
   return (
-
     <div className="w-full mx-auto p-4 lg:px-10 bg-white rounded-lg shadow-md">
-
       <div>
-
-        <button onClick={() => setIsMainOpen(!isMainOpen)} className="w-full flex justify-between items-center text-left text-[#145CA9] font-medium py-2">
+        <button
+          onClick={() => setIsMainOpen(!isMainOpen)}
+          className="w-full flex justify-between items-center text-left text-[#145CA9] font-medium py-2"
+        >
           <span className="font-bold ">Bulletin Committees</span>
-          <img src={`/assets/${isMainOpen ? "arrow-up" : "arrow-down"}.png`} alt="" className="w-7 h-7  inline-block" />
+          <img
+            src={`/assets/${isMainOpen ? "arrow-up" : "arrow-down"}.png`}
+            alt=""
+            className="w-7 h-7  inline-block"
+          />
         </button>
 
         {isMainOpen && (
           <div className="text-sm text-[#145CA9] flex flex-col items-center">
-            <div className="w-[500px]">
-              <p className="mb-4">The Bulletin Committee is responsible for publishing the club's bulletin, including:</p>
+            {/* changed nathans sizing design slightly, as the edges of the description and image were getting cut off at smaller widths below 500px */}
+            <div className="w-9/10 flex flex-col items-center">
+              <p className="mb-4">
+                The Bulletin Committee is responsible for publishing the club's
+                bulletin, including:
+              </p>
 
               <ul className="list-disc list-inside mb-4 text-left">
-                <li>Writing articles on a range of financial and economic topics</li>
+                <li>
+                  Writing articles on a range of financial and economic topics
+                </li>
                 <li>Offering advice on each other's articles</li>
-                <li>Contributing opinions towards the club's weekly magazine</li>
+                <li>
+                  Contributing opinions towards the club's weekly magazine
+                </li>
               </ul>
 
-              <div className="w-full h-[300px] bg-[#EBF7FE] flex items-center justify-center overflow-hidden rounded-lg">
+              <div className="w-full h-[300px] max-w-[500px] bg-[#EBF7FE] flex items-center justify-center overflow-hidden rounded-lg">
                 <Image
                   src="/assets/bulletin-committee.png"
-                  alt="investment-committee"
+                  alt="bulletin-committee"
                   width={500}
                   height={300}
                   className="object-cover"
@@ -42,19 +79,49 @@ const InvestmentCommittee = () => {
             </div>
 
             <div className="items-center text-center mt-4">
-              <p className="p-2"><strong>Back Row: </strong>Daniel Guonho-Song, Andy Tran, Sohum Karl, Meghana Gaddam</p>
-              <p className="p-2"><strong>Third Row: </strong>Yasmin Coombe, Skip Gee, Connor Petrie, Oliver Turnbull</p>
-              <p className="p-2"><strong>Second Row: </strong>Yao Ming Lee, Hunter Sari, Alice Hout</p>
-              <p className="p-2"><strong>Front Row: </strong>Devika Modak, Annabelle Larsen, Isabella Ho, Riley Bogard-Allen, Lisa Shiozawa, Manav Treekam</p>
-              <p className="p-2"><strong>Absent: </strong>Bella Crawford, Waejen Kwan</p>
+              <p className="p-2">
+                <strong>Back Row: </strong>Daniel Guonho-Song, Andy Tran, Sohum
+                Karl, Meghana Gaddam
+              </p>
+              <p className="p-2">
+                <strong>Third Row: </strong>Yasmin Coombe, Skip Gee, Connor
+                Petrie, Oliver Turnbull
+              </p>
+              <p className="p-2">
+                <strong>Second Row: </strong>Yao Ming Lee, Hunter Sari, Alice
+                Hout
+              </p>
+              <p className="p-2">
+                <strong>Front Row: </strong>Devika Modak, Annabelle Larsen,
+                Isabella Ho, Riley Bogard-Allen, Lisa Shiozawa, Manav Treekam
+              </p>
+              <p className="p-2">
+                <strong>Absent: </strong>Bella Crawford, Waejen Kwan
+              </p>
             </div>
-          </div>
 
+            <button
+              onClick={() => setIsMembersOpen(!isMembersOpen)}
+              className="w-full flex justify-between items-center text-left text-[#145CA9] font-medium py-2"
+            >
+              <span className="font-bold ">Team Members and Roles</span>
+              <img
+                src={`/assets/${isMembersOpen ? "arrow-up" : "arrow-down"}.png`}
+                alt=""
+                className="w-7 h-7  inline-block"
+              />
+            </button>
+
+            {isMembersOpen && (
+                <div>
+
+                </div>
+            )}
+          </div>
         )}
       </div>
 
       <hr className="border-t border-[#CBC6C6] w-9/10 self-center my-6" />
-
     </div>
   );
 };
