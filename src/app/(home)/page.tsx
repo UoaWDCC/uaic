e05634c2c64ed39d6669e49691bf819ccd8e8089
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-07-10 15:57:17 +1200

    mobile styling complete

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,13 @@
+import ArticleList from "@/components/ArticleList";
+import InvestConnectWork from "@/components/InvestConnectWork";
 
 
 export default function Home() {
   return (
     <div>
       {/* Build Homepage Here */}
+      <ArticleList/>
+      <InvestConnectWork/>
     </div>
   );
 }
---
+++ src/components/InvestConnectWork.tsx
@@ -0,0 +1,19 @@
+const InvestConnectWork = () => {
+    return ( 
+        <div className="
+            flex flex-col border-solid border-red-500 border-2 pb-[12em]
+            bg-gradient-to-t from-darkBlue via-[#89ADD4] to-[#ffffff]
+        ">
+            <h1 className="
+                text-center text-[35px] font-black leading-12 w-[10em] mx-auto
+                bg-gradient-to-r from-darkBlue to-[#5FB4FF] text-transparent bg-clip-text
+            ">Invest, Connect and Work with us</h1>
+            <button className="
+                mx-auto flex justify-center mt-[1em] font-semibold
+                bg-[#f1f8ff] text-darkBlue px-[1.8em] py-[0.3em] rounded-[1em]
+            ">Join Us</button>
+        </div>
+     );
+}
+ 
+export default InvestConnectWork;
