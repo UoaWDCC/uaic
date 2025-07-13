b36ed084a6cd8f22b390109fdfb7dcce05f86075
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-07-13 20:15:57 +1200

    remove error import

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import ArticleList from "@/components/ArticleList";
 import Footer from "@/components/Footer";
 import InvestConnectWork from "@/components/InvestConnectWork";
+import UpdatedBulletin from "@/components/UpdatedBulletin";
 
 
 
@@ -13,6 +14,7 @@ export default function Home() {
     <div>
       {/* Build Homepage Here */}
       <ArticleList/>
+      <UpdatedBulletin/>
       <InvestConnectWork/>
     </div>
   );
--- src/components/UpdatedBulletin.tsx
+++ src/components/UpdatedBulletin.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../app/(home)/globals.css"
 
 const UpdatedBulletin = () => {
     return (
