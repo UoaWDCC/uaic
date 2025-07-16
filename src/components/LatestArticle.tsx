d3d8cabe55b0c4b05b7bb971a035835c765aa977
andrecamerino <camerinoandre@gmail.com>
2025-07-16 19:28:30 +1200

    fix LatestArticle width

--- src/components/LatestArticle.tsx
+++ src/components/LatestArticle.tsx
@@ -18,14 +18,14 @@ const LatestArticle: React.FC<LatestArticleProps> = ({
   description,
   pdfUrl,
 }) => {
-
   const readMoreFunction = () => {
     window.open(pdfUrl, "_blank");
-  }
+  };
 
   return (
     <div
       className="
+        w-full
         flex flex-col items-center gap-[47px]
         p-20
         bg-[radial-gradient(70%_70%_at_50%_55%,rgba(20,92,169,0.2)_60%,rgba(255,255,255,0.2)_80%)]
@@ -54,7 +54,10 @@ const LatestArticle: React.FC<LatestArticleProps> = ({
             <h2 className="text-sm mb-2 font-light">Issue #{issueNumber}</h2>
             <h1 className="text-xl font-bold mb-4 leading-tight">{title}</h1>
             <p className="mb-6 text-darkBlue font-light">{description}</p>
-            <button onClick={readMoreFunction} className="bg-darkBlue text-white px-4 py-1 w-[224px] h-[37px] rounded-xl">
+            <button
+              onClick={readMoreFunction}
+              className="bg-darkBlue text-white px-4 py-1 w-[224px] h-[37px] rounded-xl"
+            >
               Read More
             </button>
           </div>
@@ -62,32 +65,33 @@ const LatestArticle: React.FC<LatestArticleProps> = ({
 
         {/* Small screen layout: vertical image background + gradient + text */}
         <div className="flex flex-col lg:hidden relative w-[330px] h-[561px] rounded-3xl overflow-hidden bg-white items-center gap-[14px]">
+          {/* Title */}
+          <h1 className="text-darkBlue text-xl font-bold lg:text-2xl mt-6">
+            Latest Article
+          </h1>
 
-            {/* Title */}
-            <h1 className="text-darkBlue text-xl font-bold lg:text-2xl mt-6">
-              Latest Article
-            </h1>
-
-            {/* Content */}
-            <div
-              className="relative w-[295px] h-[417px] flex flex-col gap-[9px] p-6 bg-cover bg-center text-white justify-end rounded-3xl"
-              style={{
-                backgroundImage: `linear-gradient(to top, rgba(20,92,169,1) 0%, rgba(20,92,169,1) 50%, rgba(255,255,255,0) 75%), url('${imageSrc}')`,
-              }}
-            >
-              <h2 className="text-xs font-light">Issue #{issueNumber}</h2>
-              <h1 className="font-bold text-sm leading-tight">{title}</h1>
-              <p className="font-light text-xs">{description}</p>
-            </div>
-
-            {/* Button */}
-            <button onClick={readMoreFunction} className="bg-darkBlue text-white px-4 py-1 w-[295px] h-[37px] rounded-xl hover:cursor-pointer">
-              Read More
-            </button>
+          {/* Content */}
+          <div
+            className="relative w-[295px] h-[417px] flex flex-col gap-[9px] p-6 bg-cover bg-center text-white justify-end rounded-3xl"
+            style={{
+              backgroundImage: `linear-gradient(to top, rgba(20,92,169,1) 0%, rgba(20,92,169,1) 50%, rgba(255,255,255,0) 75%), url('${imageSrc}')`,
+            }}
+          >
+            <h2 className="text-xs font-light">Issue #{issueNumber}</h2>
+            <h1 className="font-bold text-sm leading-tight">{title}</h1>
+            <p className="font-light text-xs">{description}</p>
           </div>
+
+          {/* Button */}
+          <button
+            onClick={readMoreFunction}
+            className="bg-darkBlue text-white px-4 py-1 w-[295px] h-[37px] rounded-xl hover:cursor-pointer"
+          >
+            Read More
+          </button>
         </div>
       </div>
-
+    </div>
   );
 };
 
