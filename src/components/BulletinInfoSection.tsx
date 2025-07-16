bfc01a10651c5de78a503cc357cd7e36df97e5d6
andrecamerino <camerinoandre@gmail.com>
2025-07-16 19:28:12 +1200

    add BulletinInfoSection component

---
+++ src/components/BulletinInfoSection.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import BulletinInfoCard from "./BulletinInfoCard";
+
+const BulletinInfosection = () => {
+  return (
+    <div
+      className="
+      w-full 
+      text-darkBlue
+      grid justify-between 
+      
+      gap-[60px]
+      pl-20 pr-20 pb-10
+      
+      lg:pl-30 lg:pr-30 lg:pb-20
+      lg:grid-cols-3 
+      lg:gap-[100px]
+    "
+    >
+     <BulletinInfoCard title="Investing is for Everyone" imageSrc="/assets/group-icon.png" text="Our writers study a range of degrees beyond just finance, and we are bound by our collective interest in financial markets" />
+     <BulletinInfoCard title="Education" imageSrc="/assets/grad-cap-icon.png" text="Education is one of our core values - all our analysts are supported in their roles through regular workshops and ongoing mentorship, assisting our team in making the best decisions when discussion the market" />
+     <BulletinInfoCard title="Article Frequency" imageSrc="/assets/group-icon.png" text="Articles are published on a frequent basis and sent our to all club members" />
+    </div>
+  );
+};
+
+export default BulletinInfosection;
