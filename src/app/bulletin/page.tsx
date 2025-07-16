ec01ce637eef1a78490274dee113f74c4ec6310e
andrecamerino <camerinoandre@gmail.com>
2025-07-16 19:27:45 +1200

    update bulletin page

--- src/app/bulletin/page.tsx
+++ src/app/bulletin/page.tsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from "react";
 import BulletinHeroSection from "@/components/BulletinHeroSection";
 import LatestArticle from "@/components/LatestArticle";
+import BulletinInfosection from "@/components/BulletinInfoSection";
+import UpdatedBulletin from "@/components/Bulletin";
+import ArticleList from "@/components/ArticleList";
 
 const page = () => {
   return (
-    <div>
-        <BulletinHeroSection />
-        <LatestArticle
-          issueNumber={1}
-          imageSrc="/assets/latest-article.jpg"
-          title="Exploring the Latest Trends in Technology"
-          description="Dive into the latest advancements and trends shaping the tech industry today."
-          pdfUrl="/assets/latest-article.pdf"
-        />
+    <div className="w-full flex flex-col items-center gap-[200px]">
+      <BulletinHeroSection />
+      <BulletinInfosection />
+      <LatestArticle
+        issueNumber={1}
+        imageSrc="/assets/latest-article.jpg"
+        title="Exploring the Latest Trends in Technology"
+        description="Dive into the latest advancements and trends shaping the tech industry today."
+        pdfUrl="/assets/latest-article.pdf"
+      />
+      <UpdatedBulletin />
+      <ArticleList />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
