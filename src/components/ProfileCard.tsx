c686f577ff903046f0d398b1ff943df18dfdfb94
jerry2005hn <jerry2005hn@gmail.com>
2025-07-23 21:06:07 +1200

    convert execs images to webp and create folders

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import React from "react";
 
 interface ProfileCardProps {
@@ -11,7 +12,9 @@ interface ProfileCardProps {
 const ProfileCard = ({ name, title, degree, imageSrc }: ProfileCardProps) => {
   return (
     <div className="flex flex-col items-center text-center max-w-[160px]">
-      <img
+      <Image
+        height={100}
+        width={100}
         src={imageSrc}
         alt={name}
         className="rounded-full w-32 h-32 object-cover mb-2"
