c8eeec546f0379246c0c23ccde3707bad47f99f0
Kelvin Nti Mensah <70778409+Kellyzicoder@users.noreply.github.com>
2026-04-30 14:59:32 +1200

    fix(profile-card): adjust shield spacing

--- src/components/team/KelvinCard.tsx
+++ src/components/team/KelvinCard.tsx
@@ -5,7 +5,7 @@ const KelvinCard = async () => {
   const profileUrl =
     (await getProfileCardImage("69f0707d8372c16f99240669")) || "/assets/team/KyleRayner.jpg";
   return (
-    <div className="mb-10 flex w-full max-w-[160px] flex-col items-center rounded-lg text-center sm:max-w-[200px]">
+    <div className="flex w-full max-w-[160px] flex-col items-center rounded-lg text-center sm:max-w-[200px]">
       {/* Profile photo */}
       <div className="relative mb-2 h-24 w-24 sm:h-32 sm:w-32">
         <Image
@@ -21,16 +21,16 @@ const KelvinCard = async () => {
         <p className="text-darkBlue sm:text-body text-sm font-bold">Kelvin Mensah</p>
         <p className="text-darkBlue sm:text-body text-xs">Developer</p>
         <p className="sm:text-body mt-1 text-xs">
-          ...love editing, coding and subway. Always up for a challenge.
+          Love editing, coding and subway. Always up for a challenge.
         </p>
         <p className="sm:text-body mt-2 text-xs text-gray-500 italic">&ldquo;Lock in!&rdquo;</p>
 
-        <div className="-mt-5 flex justify-end pl-30">
+        <div className="-mt-8 flex justify-end pl-15">
           <Image
             src="/assets/cap_shield.png"
             alt="Captain America Shield Image"
             width={128}
-            height={64}
+            height={128}
           />
         </div>
       </div>
