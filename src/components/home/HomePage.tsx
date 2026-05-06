74b257bb8c18746120db5eccf0fe9b11c8b80a4d
Nathan Dalpatan <nathandalp@gmail.com>
2026-05-06 14:36:07 +1200

    style: changed the background & style/positioning of the content (not exact replica yet)

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -18,37 +18,34 @@ const HomePage = () => {
 
   return (
     <>
-      <div className="relative min-h-[500px] w-full overflow-x-hidden overflow-y-hidden lg:mt-[142px] lg:min-h-[749px]">
+      <div className="relative min-h-[610px] w-full overflow-x-hidden lg:-mt-[131.75px] lg:min-h-[676px]">
         {/* Background */}
         <div
-          className="absolute top-0 left-0 z-[-2] min-h-[610px] w-screen bg-cover bg-center bg-no-repeat lg:min-h-screen"
+          className="absolute inset-0 z-[-2] bg-cover bg-[center_20%] bg-no-repeat lg:bg-[center_0%]"
           style={{
             backgroundImage: `url('${backgroundImage}')`,
           }}
         />
 
-        {/* Transparent Overlay */}
-        <div className="fixed top-0 left-0 z-[-1] h-screen w-full bg-white/50 from-white/10 to-white/0 lg:bg-[transparent] lg:bg-gradient-to-r" />
-
-        {/* Vertical gradient overlay (bottom → top) */}
-        <div className="absolute z-[-1] h-screen w-full bg-gradient-to-t from-white via-transparent via-white/100 to-white/0 lg:via-white/70" />
-
-        {/* Foreground content */}
-        <div className="relative mx-auto mt-[80px] flex h-[324] w-[305px] flex-col gap-[70px] text-center lg:mt-[118px] lg:ml-[112px] lg:h-[384] lg:w-[494px] lg:gap-[45px] lg:text-left">
-          <h1 className="from-darkBlue to-babyBlue mx-[20px] bg-gradient-to-r bg-clip-text text-[41.65px] leading-[45px] font-[900] tracking-[0px] text-transparent lg:mx-0 lg:text-[75.65px] lg:leading-[85px]">
-            Investing is for everyone
+        {/* Readability shade */}
+        <div className="absolute inset-0 z-[-1] bg-gradient-to-b from-black/15 via-black/10 to-black/25" />
+        <div className="relative mx-auto mt-[130px] flex w-full flex-col items-center gap-[20px] text-center lg:mt-[170px] lg:ml-[100px] lg:w-[1200px] lg:items-start lg:gap-[45px] lg:text-left">
+          <h1 className="mx-[30px] text-[30px] leading-[34px] font-[700] tracking-[0px] text-white lg:mx-0 lg:text-[100px] lg:leading-[120px]">
+            University Of Auckland
+            <br />
+            Investment Club
           </h1>
 
-          <h3 className="mx-[20px] text-[16px] leading-[17.84px] font-[300] tracking-[0px] lg:mx-0 lg:text-[24.3px] lg:leading-[32.39px]">
-            We welcome members from all academic backgrounds and stages at the University of
-            Auckland.
-          </h3>
+          <div className="flex flex-col gap-14 lg:flex-row lg:gap-50">
+            <h3 className="mx-[20px] text-[20px] leading-[24px] font-[350] tracking-[0px] text-white lg:mx-0 lg:text-[16px] lg:leading-[100%]">
+              A student-run investing society. Welcoming members from all academic backgrounds and
+              stages at the University of Auckland.
+            </h3>
 
-          <div className="flex h-[53px] w-[320px] gap-[8px]">
-            <Button link="/about" variant="secondary">
-              About Us
-            </Button>
-            <Button link="/joinus">Join Us</Button>
+            {/* Button inline, not absolute */}
+            <div className="mx-auto h-[50px] w-[240px] lg:mx-0 lg:w-[320px]">
+              <Button link="/joinus">Become a Member</Button>
+            </div>
           </div>
         </div>
       </div>
