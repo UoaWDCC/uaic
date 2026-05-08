a0bb07b3eb58563150541ac552a34decc3e06819
maryzenn <mzmarikit@gmail.com>
2026-05-08 17:20:05 +1200

    idk what happened but i fixed it.

--- src/components/InvestConnectWork.tsx
+++ src/components/InvestConnectWork.tsx
@@ -2,12 +2,12 @@ import Button from "./Button";
 
 const InvestConnectWork = () => {
   return (
-    <div className="m-5 mx-auto mb-10 w-full md:h-[200px] lg:h-[300px]">
-      <div className="mx-auto flex max-w-5/6 flex-col items-center justify-center rounded-xl bg-white p-14 shadow-md">
-        <h1 className="mx-auto w-[10em] shrink bg-clip-text text-center text-[1.5rem] leading-15 font-[700] text-[#145BA7] md:text-[3rem] md:leading-25 lg:text-[5rem]">
+    <div className="m-5 mx-auto mb-10 w-full">
+      <div className="mx-auto flex max-w-5/6 flex-col items-center justify-center rounded-xl bg-white shadow-md md:p-11 lg:p-14">
+        <h1 className="p-5 text-center text-[clamp(1rem,5.7vw,8rem)] leading-snug font-[700] text-[#145BA7]">
           Invest, Connect and Work with us.
         </h1>
-        <div className="flex w-full justify-center pt-10">
+        <div className="flex w-full justify-center pb-5">
           <Button link="/joinus" defaultSize={true}>
             Join Us
           </Button>
