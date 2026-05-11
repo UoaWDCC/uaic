a3f4de8b82295f298d76127fb2dfec679f1e57bb
maryzenn <mzmarikit@gmail.com>
2026-05-12 10:32:47 +1200

    desktop spacing fixed. need to make sure mobile view is okay

--- src/components/InvestConnectWork.tsx
+++ src/components/InvestConnectWork.tsx
@@ -1,16 +1,16 @@
-import Button from "./MemberSignupButton";
+import MemberSignupButton from "./MemberSignupButton";
 
 const InvestConnectWork = () => {
   return (
-    <div className="m-5 mx-auto mb-10 w-full">
-      <div className="mx-auto flex max-w-5/6 flex-col items-center justify-center rounded-xl bg-white shadow-md md:p-11 lg:p-14">
+    <div className="mx-auto mt-10 w-full max-w-[1236px] shrink lg:mt-20 lg:mb-20">
+      <div className="mx-auto flex flex-col items-center justify-center rounded-xl bg-white shadow-md md:p-11 lg:p-14">
         <h1 className="p-5 text-center text-[clamp(1rem,5.7vw,8rem)] leading-snug font-[700] text-[#145BA7]">
-          Invest, Connect and Work with us.
+          Invest, Connect And Work With Us.
         </h1>
         <div className="flex w-full justify-center pb-5">
-          <Button link="/joinus" defaultSize={true}>
+          <MemberSignupButton link="/joinus" defaultSize={true}>
             Join Us
-          </Button>
+          </MemberSignupButton>
         </div>
       </div>
     </div>
