966a409ce004a765b919ef886279e85eddb7a840
Andre Camerino <camerinoandre@gmail.com>
2026-05-23 14:49:45 +1200

    add comments for how to test

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,3 +1,11 @@
+// To test email, run the following:
+// npx tsx src/test-email.ts (assuming dotenv is already installed via pnpm)
+// This sends test-email.ts at src/test-email.ts
+// Output shoult look like this:
+// dev@MacBook-Pro-6 uaic % npx tsx src/test-email.ts
+// Sender: onboarding@resend.dev, Recipient: (our dev email)
+// Email sent successfully.
+
 import { sendEmail } from "@/lib/send-email";
 
 export async function POST(request: Request) {
