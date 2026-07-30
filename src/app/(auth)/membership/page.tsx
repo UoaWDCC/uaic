a70bda52dade80514f6c2af2a77e041506a34c51
Aeryn Elizabeth Lao <aeryn.elizabeth.sy.lao@gmail.com>
2026-07-31 11:47:53 +1200

    refactor: moved signup form page into auth route group

---
+++ src/app/(auth)/membership/page.tsx
@@ -0,0 +1,7 @@
+export default function MembershipPage() {
+  return (
+    <div className="min-h-screen bg-linear-to-r from-[#249aff] to-[#005eaf]">
+      <div className="flex justify-center px-6"></div>
+    </div>
+  );
+}
