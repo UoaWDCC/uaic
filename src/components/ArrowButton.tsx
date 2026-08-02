12f2232433e67779ad9622f136564b2a1d108dda
Luca Devoti <devotiluca1@gmail.com>
2026-08-02 14:09:11 +1200

    fix: dynamic button width based on text length

--- src/components/ArrowButton.tsx
+++ src/components/ArrowButton.tsx
@@ -10,7 +10,7 @@ const ArrowButton = ({ variant = "member" }: ArrowButtonProps) => {
     <Link
       href={variant === "home" ? "/" : "/joinus"}
       data-variant={variant}
-      className="group relative flex h-[37px] w-fit items-center justify-center gap-3 overflow-hidden rounded-full bg-white py-2.5 pr-4.5 pl-4.5 whitespace-nowrap text-white transition-colors duration-200 hover:text-[#005EAF] sm:h-[51px] sm:w-[249.36px] sm:gap-[8px] sm:py-0 sm:pr-[16px] sm:pl-[10px] 2xl:h-[86px] 2xl:w-[430px] 2xl:gap-5 2xl:pr-8 2xl:pl-6"
+      className="group relative flex h-[37px] w-fit items-center justify-center gap-3 overflow-hidden rounded-full bg-white px-4.5 py-2.5 whitespace-nowrap text-white transition-colors duration-200 hover:text-[#005EAF] sm:h-[51px] sm:gap-[8px] sm:px-[16px] sm:py-0 2xl:h-[86px] 2xl:gap-5 2xl:px-8"
     >
       <span className="absolute inset-0 rounded-full bg-gradient-to-l from-[#005EAF] to-[#249AFF] transition-opacity duration-200 group-hover:opacity-0" />
 
