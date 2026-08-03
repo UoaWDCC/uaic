11679f61fac1ba12cfec92ef64e3444539ea170f
Evan Au <93043846+evanautianle@users.noreply.github.com>
2026-08-03 20:12:33 +1200

    updated links that lead to joinus lead to login instead

--- src/components/MemberSignupButton.tsx
+++ src/components/MemberSignupButton.tsx
@@ -3,7 +3,7 @@ import { GoArrowUpRight } from "react-icons/go";
 const MemberSignupButton = () => {
   return (
     <Link
-      href="/joinus"
+      href="/login"
       className="group relative flex h-[37px] w-fit items-center justify-center gap-3 overflow-hidden rounded-full bg-white py-2.5 pr-4.5 pl-4.5 whitespace-nowrap text-white transition-colors duration-200 hover:text-[#005EAF] sm:h-[51px] sm:w-[249.36px] sm:gap-[8px] sm:py-0 sm:pr-[16px] sm:pl-[10px] 2xl:h-[86px] 2xl:w-[430px] 2xl:gap-5 2xl:pr-8 2xl:pl-6"
     >
       <span className="absolute inset-0 rounded-full bg-gradient-to-l from-[#005EAF] to-[#249AFF] transition-opacity duration-200 group-hover:opacity-0" />
