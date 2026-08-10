191d2f4ec65602382dfc715cb22d1cb2d0d5f4fe
evanautianle <evan.au206@gmail.com>
2026-08-10 14:58:51 +1200

    remove articlelist and bulletin from homepage, adjusted margins to fix style

--- src/components/EventsSection.tsx
+++ src/components/EventsSection.tsx
@@ -2,18 +2,19 @@
 import React, { useState } from "react";
 import RecentEvents from "@/components/events/RecentEvents";
 import UpcomingEvents from "@/components/events/UpcomingEvents";
+import type { Event } from "../../payload-types";
 
 interface EventsSectionProps {
-  upcomingEvents: any[];
-  recentEvents: any[];
+  upcomingEvents: Event[];
+  recentEvents: Event[];
 }
 
 const EventsSection = ({ upcomingEvents, recentEvents }: EventsSectionProps) => {
   const [selected, setSelected] = useState("upcoming");
 
   return (
-    <div className="mt-[200px] flex flex-col items-center gap-6 bg-[radial-gradient(circle,_var(--darkBlue)_0%,_white_42%)] px-[16px] lg:px-[120px]">
-      <div className="mt-[80px] flex w-fit rounded-full bg-gradient-to-r from-white to-white">
+    <div className="flex flex-col items-center gap-6 bg-[radial-gradient(circle,_var(--darkBlue)_0%,_white_42%)] px-[16px] lg:px-[120px]">
+      <div className="flex w-fit rounded-full bg-gradient-to-r from-white to-white">
         <button
           onClick={() => setSelected("upcoming")}
           className={`rounded-full px-6 py-2 font-medium transition-all duration-600 hover:cursor-pointer ${selected === "upcoming" ? "bg-[var(--darkBlue)] text-white" : "bg-white text-black"} `}
