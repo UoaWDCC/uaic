638b762ae552798da5276d941eda7bc1f7a9f5a9
Andre Camerino <camerinoandre@gmail.com>
2026-08-17 13:28:38 +1200

    feat: seperate logos to different bars via partiy

--- src/components/home/SponsorsBanner.tsx
+++ src/components/home/SponsorsBanner.tsx
@@ -6,6 +6,9 @@ import { getSponsors } from "@/features/home/data/getSponsors";
 const SponsorsBanner = async () => {
   const sponsors = await getSponsors();
 
+  const bar1 = sponsors.filter((_, index) => index % 2 === 0);
+  const bar2 = sponsors.filter((_, index) => index % 2 !== 0);
+
   return (
     <div className="relative h-full overflow-hidden bg-white py-8 lg:h-full lg:py-14">
       <h3 className="text-darkBlue mt-[0px] text-center text-[19.04px] leading-[19.04px] font-[300] lg:mt-[40px] lg:text-[28px] lg:leading-[22px]">
@@ -13,7 +16,7 @@ const SponsorsBanner = async () => {
       </h3>
       <Marquee speed={120} autoFill gradient={true} gradientColor="#ffffff">
         <div className="mt-10 flex items-center justify-evenly gap-4 px-4 lg:gap-8">
-          {sponsors.map((sponsor) => (
+          {bar1.map((sponsor) => (
             <div key={sponsor.id} className="relative flex h-20 w-32 items-center">
               <LogoTint src={sponsor.logo} colorClass="bg-[#145ca9]" className="h-full w-full" />
             </div>
@@ -22,7 +25,7 @@ const SponsorsBanner = async () => {
       </Marquee>
       <Marquee speed={120} autoFill gradient={true} direction="right" gradientColor="#ffffff">
         <div className="mt-10 mb-4 flex items-center justify-evenly gap-4 px-4 lg:mb-6 lg:gap-8">
-          {sponsors.map((sponsor) => (
+          {bar2.map((sponsor) => (
             <div key={sponsor.id} className="relative flex h-20 w-32 items-center">
               <LogoTint src={sponsor.logo} colorClass="bg-[#145ca9]" className="h-full w-full" />
             </div>
