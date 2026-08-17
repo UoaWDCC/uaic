4250be8e35a55c4c2cb48cf5ecbdc765b0ba7dd1
Andre Camerino <camerinoandre@gmail.com>
2026-08-17 13:51:13 +1200

    fix: adjust logo gap

--- src/components/home/SponsorsBanner.tsx
+++ src/components/home/SponsorsBanner.tsx
@@ -3,6 +3,8 @@ import Marquee from "react-fast-marquee";
 import LogoTint from "./LogoTint";
 import { getSponsors } from "@/features/home/data/getSponsors";
 
+const sponsorBarStylings = "mt-10 flex items-center justify-evenly gap-[36px] px-4 lg:gap-[64px]";
+
 const SponsorsBanner = async () => {
   const sponsors = await getSponsors();
 
@@ -10,12 +12,12 @@ const SponsorsBanner = async () => {
   const bar2 = sponsors.filter((_, index) => index % 2 !== 0);
 
   return (
-    <div className="relative h-full overflow-hidden bg-white py-8 lg:h-full lg:py-14">
+    <div className="relative mb-4 h-full overflow-hidden bg-white py-8 lg:h-full lg:py-14">
       <h3 className="text-darkBlue mt-[0px] text-center text-[19.04px] leading-[19.04px] font-[300] lg:mt-[40px] lg:text-[28px] lg:leading-[22px]">
         Our 2026 Sponsors and Partners
       </h3>
       <Marquee speed={120} autoFill gradient={true} gradientColor="#ffffff">
-        <div className="mt-10 flex items-center justify-evenly gap-8 px-4 lg:gap-12">
+        <div className={`${sponsorBarStylings}`}>
           {bar1.map((sponsor) => (
             <div
               key={sponsor.id}
@@ -27,13 +29,13 @@ const SponsorsBanner = async () => {
                     : "1 / 1",
               }}
             >
-              <LogoTint src={sponsor.logo} colorClass="bg-[#145ca9]" className="h-full w-full" />
+              <LogoTint src={sponsor.logo} colorClass="bg-darkBlue" className="h-full w-full" />
             </div>
           ))}
         </div>
       </Marquee>
       <Marquee speed={120} autoFill gradient={true} direction="right" gradientColor="#ffffff">
-        <div className="mt-10 mb-4 flex items-center justify-evenly gap-8 px-4 lg:mb-6 lg:gap-12">
+        <div className={`${sponsorBarStylings}`}>
           {bar2.map((sponsor) => (
             <div
               key={sponsor.id}
@@ -45,7 +47,7 @@ const SponsorsBanner = async () => {
                     : "1 / 1",
               }}
             >
-              <LogoTint src={sponsor.logo} colorClass="bg-[#145ca9]" className="h-full w-full" />
+              <LogoTint src={sponsor.logo} colorClass="bg-darkBlue" className="h-full w-full" />
             </div>
           ))}
         </div>
