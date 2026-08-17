66e5d15deac0cf5ca1304a1819b13f42f6e65f1d
Kelvin Nti Mensah <70778409+Kellyzicoder@users.noreply.github.com>
2026-08-17 15:31:49 +1200

    added a fetch article for fetching backend

---
+++ src/lib/fetchArticles.ts
@@ -0,0 +1,10 @@
+export async function fetchArticles(params?: Record<string, string>) {
+  const query = params ? "?" + new URLSearchParams(params).toString() : "";
+
+  const response = await fetch(`/api/articles${query}`, { cache: "no-store" });
+
+  if (!response.ok) return [];
+
+  const data = await response.json();
+  return data?.docs || [];
+}
