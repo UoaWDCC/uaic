730c8038025cbc41c3114ef8848a0a353ad0ab05
maryzenn <mzmarikit@gmail.com>
2026-08-17 15:01:46 +0800

    desktop view done for membership and event email. need to change button

--- src/components/EventEmail.tsx
+++ src/components/EventEmail.tsx
@@ -15,37 +15,35 @@ const EventEmail = () => {
       <div className="sm:py-28px flex justify-center px-6">
         <div className="flex min-h-[600px] w-full max-w-[472px] flex-col gap-[30px] rounded-[8px] bg-white px-[28px] py-[30px] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
           <div className="w-full flex-col gap-[8px]">
-            <div className="w-full text-[5vw] font-semibold text-[#00529B] lg:text-[2.2vw]">
+            <div className="w-full text-[5vw] font-semibold text-[#00529B] md:text-[1.7vw]">
               You're all set!
             </div>
-            <div className="w-full text-[4vw] text-[rgba(0,0,0,0.20)] lg:text-[1.7vw]">
+            <div className="w-full text-[4vw] text-[rgba(0,0,0,0.20)] md:text-[1.2vw]">
               Your attendance has been confirmed.
             </div>
           </div>
           {/* intro */}
-          <div className="w-full text-[3vw] text-black lg:text-[1.5vw]">
+          <div className="w-full text-[3vw] text-black md:text-[1vw]">
             Hi [First Name],
             <p>
               <br />
               You've successfully signed up for one of our upcoming events and your name has been
-              added to our list of attendees. We can’t wait to see you there.
+              added to our list of attendees. We can't wait to see you there.
             </p>
           </div>
           {/* Membership Details */}
           <div className="w-full flex-col gap-[24px]">
-            <div className="lg:text-[1.5vw font-600 gap-[24px] text-[3.5vw] text-[#249AFF]">
+            <div className="font-600 gap-[24px] text-[3.5vw] text-[#249AFF] md:text-[1.3vw]">
               EVENT DETAILS
               <div className="w-full flex-row">
                 <div className="flex w-full justify-between pt-5">
-                  <div className="left-0 text-[3vw] text-[#A1A1A1] lg:text-[1.vw]">Event Name</div>
-                  <div className="left-0 text-[3vw] text-[#005EAF] lg:text-[1.vw]">
-                    [Event Name]
-                  </div>
+                  <div className="left-0 text-[3vw] text-[#A1A1A1] md:text-[1vw]">Event Name</div>
+                  <div className="left-0 text-[3vw] text-[#005EAF] md:text-[1vw]">[Event Name]</div>
                 </div>
                 <div className="h-[24px] w-full border-b-[0.8px] border-[#A1A1A1] bg-white"></div>
                 <div className="flex w-full justify-between pt-4">
-                  <div className="left-0 text-[3vw] text-[#A1A1A1] lg:text-[1.vw]">Date</div>
-                  <div className="left-0 text-right text-[3vw] text-[#005EAF] lg:text-[1.vw]">
+                  <div className="left-0 text-[3vw] text-[#A1A1A1] md:text-[1vw]">Date</div>
+                  <div className="left-0 text-right text-[3vw] text-[#005EAF] md:text-[1vw]">
                     [Date]
                     <p>[Start Time - End Time]</p>
                   </div>
@@ -53,8 +51,8 @@ const EventEmail = () => {
                 <div className="h-[24px] w-full border-b-[0.8px] border-[#A1A1A1] bg-white"></div>
               </div>
               <div className="flex w-full justify-between pt-4">
-                <div className="left-0 text-[3vw] text-[#A1A1A1] lg:text-[1.vw]">Location</div>
-                <div className="left-0 text-[3vw] text-[#005EAF] lg:text-[1.vw]">[Address]</div>
+                <div className="left-0 text-[3vw] text-[#A1A1A1] md:text-[1vw]">Location</div>
+                <div className="left-0 text-[3vw] text-[#005EAF] md:text-[1vw]">[Address]</div>
               </div>
               <div className="h-[24px] w-full border-b-[0.8px] border-[#A1A1A1] bg-white"></div>
             </div>
@@ -62,30 +60,30 @@ const EventEmail = () => {
           {/* Next steps section */}
           <div>
             <div className="gap-[24px] pb-4 text-[3.5vw] text-[#249AFF]">
-              <div className="font-600 pb-5 lg:text-[1.5vw]"> What's next?</div>
+              <div className="font-600 pb-5 md:text-[1.3vw]"> What's next?</div>
               <div className="w-full flex-row justify-center">
                 {/* one */}
                 <div className="flex w-full flex-row items-start gap-3 pb-3">
-                  <div className="flex h-[20px] w-[20px] flex-shrink-0 items-center justify-center rounded-full bg-[#249AFF] text-[2.5vw] font-semibold text-white md:text-base">
+                  <div className="flex h-[6vw] w-[6vw] flex-shrink-0 items-center justify-center rounded-full bg-[#249AFF] text-[2.5vw] font-semibold text-white md:h-[2vw] md:w-[2vw] md:text-base">
                     1
                   </div>
                   <div className="flex flex-col">
-                    <div className="w-full text-[3vw] text-black md:text-lg">Save the date</div>
-                    <div className="w-full text-[2.5vw] text-black opacity-50 md:text-sm">
+                    <div className="w-full text-[3vw] text-black md:text-[1vw]">Save the date</div>
+                    <div className="w-full text-[2.5vw] text-black opacity-50 md:text-[0.9vw]">
                       Mark your calendars, organise your schedules and find the location, if needed.
                     </div>
                   </div>
                 </div>
                 {/* two */}
                 <div className="flex w-full flex-row items-start gap-3 pb-3">
-                  <div className="flex h-[20px] w-[20px] flex-shrink-0 items-center justify-center rounded-full bg-[#249AFF] text-[2.5vw] font-semibold text-white md:text-base">
+                  <div className="flex h-[6vw] w-[6vw] flex-shrink-0 items-center justify-center rounded-full bg-[#249AFF] text-[2.5vw] font-semibold text-white md:h-[2vw] md:w-[2vw] md:text-base">
                     2
                   </div>
                   <div className="flex flex-col">
-                    <div className="w-full text-[3vw] text-black md:text-lg">
-                      Can’t make it anymore?
+                    <div className="w-full text-[3vw] text-black md:text-[1vw]">
+                      Can't make it anymore?
                     </div>
-                    <div className="w-full text-[2.5vw] text-black opacity-50 md:text-sm">
+                    <div className="w-full text-[2.5vw] text-black opacity-50 md:text-[0.9vw]">
                       We totally understand — just make sure to cancel your spot by emailing us, so
                       we can offer your spot to someone else.
                     </div>
@@ -93,26 +91,26 @@ const EventEmail = () => {
                 </div>
                 {/* three */}
                 <div className="flex w-full flex-row items-start gap-3 pb-3">
-                  <div className="flex h-[20px] w-[20px] flex-shrink-0 items-center justify-center rounded-full bg-[#249AFF] text-[2.5vw] font-semibold text-white md:text-base">
+                  <div className="flex h-[6vw] w-[6vw] flex-shrink-0 items-center justify-center rounded-full bg-[#249AFF] text-[2.5vw] font-semibold text-white md:h-[2vw] md:w-[2vw] md:text-base">
                     3
                   </div>
                   <div className="flex flex-col">
-                    <div className="w-full text-[3vw] text-black md:text-lg">
+                    <div className="w-full text-[3vw] text-black md:text-[1vw]">
                       Keep an eye on your inbox
                     </div>
-                    <div className="w-full text-[2.5vw] text-black opacity-50 md:text-sm">
+                    <div className="w-full text-[2.5vw] text-black opacity-50 md:text-[0.9vw]">
                       We may email you additional details closer to the event.
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="mx-auto my-auto flex justify-center [&_a]:px-26 [&_a]:py-4">
+            <div className="mx-auto my-auto flex justify-center [&_a]:px-26 [&_a]:py-4 md:[&_a]:px-[15vw]">
               <MemberSignupButton />
             </div>
           </div>
           {/* email */}
-          <div className="w-full text-[2.2vw] text-[#99A1AF]">
+          <div className="w-full text-[2.2vw] text-[#99A1AF] md:text-[0.7vw]">
             Any questions? Reach us at hello@uaic.com or find us on campus.
             <p>
               <br />— The UAIC team
